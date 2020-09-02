94adb6329fc0cf19cb3e17bdc4d2b67e83cc47c2
Igor Pochatkin <mail501@mail.ru>
2020-09-02 19:57:29 +0300

    Added task 2

--- lesson_2.py
+++ lesson_2.py
@@ -3,3 +3,10 @@
 newlist = ['spring',2, False, 3.141593]
 for eltype in newlist:
     print(eltype,type(eltype))
+
+    #exercise #2 Поменять местам
+
+my_list = [int(a) for a in input('Значения через пробел, по oкончании ввода нажмите Enter').split()]
+for el in range(0,len(my_list)-1,2):
+    my_list[el],my_list[el+1] = my_list[el+1],my_list[el]
+print(my_list)
